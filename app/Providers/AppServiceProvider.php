1795c0a8aa18bf1e7d8a22a58e83a117b0b10761
usmankamayl <usman.kamailaev@yandex.ru>
2023-10-14 07:04:52 +0300

    init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Kernel;
+use Carbon\CarbonInterval;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Model::preventLazyLoading(!app()->isProduction());
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+        DB::whenQueryingForLongerThan(500, function (Connection $connection, QueryExecuted $event) {
+            logger()
+                ->channel('telegram')
+                ->debug('whenRequestLifecycleIsLongerThan: ' . $connection->query()->toSql());
+        });
+
+        $kernel = app(Kernel::class);
+
+        $kernel->whenRequestLifecycleIsLongerThan(
+            CarbonInterval::seconds(4),
+            function () {
+                logger()
+                    ->channel('telegram')
+                    ->debug('whenRequestLifecycleIsLongerThan: ' . request()->url());
+            }
+        );
+    }
+}
